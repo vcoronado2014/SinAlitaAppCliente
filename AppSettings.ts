ca5fc011355d322632d96e86a4ff43f66f5c4e9c
vcoronado <vcoronado@saydex.cl>
2017-11-01 22:27:37 -0300

    cambios resources

--- AppSettings.ts
+++ AppSettings.ts
@@ -2,8 +2,8 @@
  * Created by VICTOR CORONADO on 19/10/2017.
  */
 export class AppSettings {
-  //public static  URL_API = 'http://apisinalitas.cpas.cl/api/';
-  public static  URL_API = 'http://localhost:31925/api/';
-  public static  URL_FOTOS = 'http://localhost:31925/';
-  //public static  URL_FOTOS = 'http://apisinalitas.cpas.cl/';
+  public static  URL_API = 'http://apisinalitas.cpas.cl/api/';
+  //public static  URL_API = 'http://localhost:31925/api/';
+  //public static  URL_FOTOS = 'http://localhost:31925/';
+  public static  URL_FOTOS = 'http://apisinalitas.cpas.cl/';
 }
